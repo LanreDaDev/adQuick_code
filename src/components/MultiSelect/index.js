bef79ba2f5644a533d30a0e4b130fd5a829cbe68
Lanre Adebayo <lanre@olade.com>
2022-04-10 19:45:13 -0600

    added a checkmark svg and added a selected index funtionality

--- src/components/MultiSelect/index.js
+++ src/components/MultiSelect/index.js
@@ -1,7 +1,32 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { ReactComponent as CheckMark } from "./checkmark.svg";
 function MultiSelect() {
+  const Options = [
+    "Airport",
+    "Alternative",
+    "Billboard",
+    "Retail",
+    "Sports Venues",
+    "Street Furniture",
+    "Transit",
+    "Wallscape",
+    "Wildposting",
+  ];
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedIndexes, setSelectedIndexes] = useState([]);
+  useEffect(() => {
+    console.log(selectedIndexes);
+  }, [selectedIndexes]);
+
+  const updateSelectedIndexes = (index) => {
+    if (selectedIndexes.includes(index)) {
+      setSelectedIndexes(
+        selectedIndexes.filter((indexed) => indexed !== index)
+      );
+    } else {
+      setSelectedIndexes([...selectedIndexes, index]);
+    }
+  };
   const isOpenStatus = () => {
     if (isOpen) {
       setIsOpen(false);
@@ -9,10 +34,30 @@ function MultiSelect() {
       setIsOpen(true);
     }
   };
+  const buttonClassName = isOpen
+    ? "MultiSelect_Component__isOpen"
+    : "MultiSelect_Component__isClose";
+
+  const OptionsComponent = Options.map((option, index) => (
+    <div
+      className="option"
+      onClick={() => {
+        updateSelectedIndexes(option);
+      }}
+      key={index}
+    >
+      {option}
+      {selectedIndexes.includes(option) ? <CheckMark /> : null}
+    </div>
+  ));
+
   return (
-    <button className={isOpen ? "isOpen" : "isClosed"} onClick={isOpenStatus}>
-      Media Type
-    </button>
+    <div className="MultiSelect_Component">
+      <button className={buttonClassName} onClick={isOpenStatus}>
+        Media Type
+      </button>
+      <div className="MultiSelect_Component__Options">{OptionsComponent}</div>
+    </div>
   );
 }
 
